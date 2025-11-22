ab75450361130c66c633e05e4bd0a0805f2fd7a2
BruhXChill <97419533+BruhXChill@users.noreply.github.com>
2025-11-21 20:52:43 -0800

    Update and rename smallRedV2.java to smallRed.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/smallRed.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/smallRed.java
@@ -1,4 +1,4 @@
-package org.firstinspires.ftc.teamcode.auto;
+package org.firstinspires.ftc.teamcode.pedroPathing;
 
 import com.bylazar.configurables.annotations.Configurable;
 import com.bylazar.telemetry.PanelsTelemetry;
@@ -13,25 +13,25 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import org.firstinspires.ftc.teamcode.pedroPathing.Constants;
 
-@Autonomous(name = "smallRedV2", group = "Autonomous")
+@Autonomous(name = "smallRed", group = "Autonomous")
 @Configurable // Panels
-public class smallRedV2 extends OpMode {
+public class smallRed extends OpMode {
     private TelemetryManager panelsTelemetry; // Panels Telemetry instance
     public Follower follower; // Pedro Pathing follower instance
     private int pathState; // Current autonomous path state (state machine)
     private Paths paths; // Paths defined in the Paths class
     private Timer pathTimer, actionTimer, opmodeTimer;
-
     @Override
     public void init() {
         panelsTelemetry = PanelsTelemetry.INSTANCE.getTelemetry();
 
         follower = Constants.createFollower(hardwareMap);
-        follower.setStartingPose(new Pose(88, 8, Math.toRadians(90)));
+        follower.setStartingPose(new Pose(88, 8, Math.toRadians(270)));
 
         paths = new Paths(follower); // Build paths
 
         panelsTelemetry.debug("Status", "Initialized");
+        panelsTelemetry.update(telemetry);
 
         pathTimer = new Timer();
         opmodeTimer = new Timer();
@@ -63,22 +63,43 @@ public class smallRedV2 extends OpMode {
         public PathChain Path7;
         public PathChain Path8;
         public PathChain Path9;
+        public PathChain Path10;
+        public PathChain Path11;
+        public PathChain Path12;
+        public PathChain Path13;
 
         public Paths(Follower follower) {
             Path1 = follower
                     .pathBuilder()
                     .addPath(
+                            new BezierLine(new Pose(88.000, 8.000), new Pose(88.000, 12.000))
+                    )
+                    .setLinearHeadingInterpolation(Math.toRadians(270), Math.toRadians(288))
+                    .build();
+
+            Path2 = follower
+                    .pathBuilder()
+                    .addPath(
                             new BezierCurve(
-                                    new Pose(88.000, 8.000),
-                                    new Pose(88.150, 24.600),
-                                    new Pose(86.250, 35.850),
-                                    new Pose(96.000, 36.000)
+                                    new Pose(88.000, 12.000),
+                                    new Pose(93.600, 29.200),
+                                    new Pose(85.200, 36.120),
+                                    new Pose(72.000, 36.000)
                             )
+                    )
+                    .setTangentHeadingInterpolation()
+                    .setReversed()
+                    .build();
+
+            Path3 = follower
+                    .pathBuilder()
+                    .addPath(
+                            new BezierLine(new Pose(72.000, 36.000), new Pose(96.000, 36.000))
                     )
                     .setTangentHeadingInterpolation()
                     .build();
 
-            Path2 = follower
+            Path4 = follower
                     .pathBuilder()
                     .addPath(
                             new BezierLine(new Pose(96.000, 36.000), new Pose(120.000, 36.000))
@@ -86,29 +107,34 @@ public class smallRedV2 extends OpMode {
                     .setTangentHeadingInterpolation()
                     .build();
 
-            Path3 = follower
+            Path5 = follower
                     .pathBuilder()
                     .addPath(
                             new BezierCurve(
                                     new Pose(120.000, 36.000),
-                                    new Pose(79.650, 36.000),
-                                    new Pose(91.000, 49.500),
-                                    new Pose(96.000, 60.000)
+                                    new Pose(91.400, 36.460),
+                                    new Pose(77.000, 76.100),
+                                    new Pose(84.000, 84.000)
                             )
                     )
                     .setTangentHeadingInterpolation()
                     .setReversed()
                     .build();
 
-            Path4 = follower
+            Path6 = follower
                     .pathBuilder()
                     .addPath(
-                            new BezierLine(new Pose(96.000, 60.000), new Pose(96.000, 60.000))
+                            new BezierCurve(
+                                    new Pose(84.000, 84.000),
+                                    new Pose(74.270, 72.590),
+                                    new Pose(79.480, 60.150),
+                                    new Pose(96.000, 60.000)
+                            )
                     )
-                    .setLinearHeadingInterpolation(Math.toRadians(-116), Math.toRadians(0))
+                    .setTangentHeadingInterpolation()
                     .build();
 
-            Path5 = follower
+            Path7 = follower
                     .pathBuilder()
                     .addPath(
                             new BezierLine(new Pose(96.000, 60.000), new Pose(120.000, 60.000))
@@ -116,42 +142,60 @@ public class smallRedV2 extends OpMode {
                     .setTangentHeadingInterpolation()
                     .build();
 
-            Path6 = follower
+            Path8 = follower
+                    .pathBuilder()
+                    .addPath(
+                            new BezierLine(new Pose(120.000, 60.000), new Pose(96.000, 60.000))
+                    )
+                    .setTangentHeadingInterpolation()
+                    .setReversed()
+                    .build();
+
+            Path9 = follower
                     .pathBuilder()
                     .addPath(
                             new BezierCurve(
-                                    new Pose(120.000, 60.000),
-                                    new Pose(79.650, 60.000),
-                                    new Pose(89.000, 75.000),
-                                    new Pose(96.000, 84.000)
+                                    new Pose(96.000, 60.000),
+                                    new Pose(79.480, 60.150),
+                                    new Pose(74.270, 72.590),
+                                    new Pose(84.000, 84.000)
                             )
                     )
                     .setTangentHeadingInterpolation()
                     .setReversed()
                     .build();
 
-            Path7 = follower
+            Path10 = follower
                     .pathBuilder()
                     .addPath(
-                            new BezierLine(new Pose(96.000, 84.000), new Pose(96.000, 84.000))
+                            new BezierLine(new Pose(84.000, 84.000), new Pose(84.000, 84.000))
                     )
-                    .setLinearHeadingInterpolation(Math.toRadians(-128), Math.toRadians(0))
+                    .setLinearHeadingInterpolation(Math.toRadians(230), Math.toRadians(180))
                     .build();
 
-            Path8 = follower
+            Path11 = follower
                     .pathBuilder()
                     .addPath(
-                            new BezierLine(new Pose(96.000, 84.000), new Pose(120.000, 84.000))
+                            new BezierLine(new Pose(84.000, 84.000), new Pose(120.000, 84.000))
                     )
                     .setTangentHeadingInterpolation()
                     .build();
 
-            Path9 = follower
+            Path12 = follower
                     .pathBuilder()
                     .addPath(
-                            new BezierLine(new Pose(120.000, 84.000), new Pose(96.000, 84.000))
+                            new BezierLine(new Pose(120.000, 84.000), new Pose(84.000, 84.000))
                     )
-                    .setLinearHeadingInterpolation(Math.toRadians(0), Math.toRadians(-128))
+                    .setTangentHeadingInterpolation()
+                    .setReversed()
+                    .build();
+
+            Path13 = follower
+                    .pathBuilder()
+                    .addPath(
+                            new BezierLine(new Pose(84.000, 84.000), new Pose(84.000, 84.000))
+                    )
+                    .setLinearHeadingInterpolation(Math.toRadians(180), Math.toRadians(230))
                     .build();
         }
     }
@@ -252,19 +296,45 @@ public class smallRedV2 extends OpMode {
             case 9:
                 /* This case checks the robot's position and will wait until the robot position is close (1 inch away) from the pickup3Pose's position */
                 if(!follower.isBusy()) {
+
+                    follower.followPath(paths.Path10);
+                    setPathState(10);
+                }
+                break;
+            case 10:
+                /* This case checks the robot's position and will wait until the robot position is close (1 inch away) from the pickup3Pose's position */
+                if(!follower.isBusy()) {
+
+                    follower.followPath(paths.Path11);
+                    setPathState(11);
+                }
+                break;
+            case 11:
+                /* This case checks the robot's position and will wait until the robot position is close (1 inch away) from the pickup3Pose's position */
+                if(!follower.isBusy()) {
+
+                    follower.followPath(paths.Path12);
+                    setPathState(12);
+                }
+                break;
+            case 12:
+                /* This case checks the robot's position and will wait until the robot position is close (1 inch away) from the pickup3Pose's position */
+                if(!follower.isBusy()) {
+                    follower.followPath(paths.Path13);
+                    setPathState(13);
+                }
+                break;
+            case 13:
+                /* This case checks the robot's position and will wait until the robot position is close (1 inch away) from the pickup3Pose's position */
+                if(!follower.isBusy()) {
                     setPathState(-1);
                 }
                 break;
         }
-        // Add your state machine Here
-        // Access paths with paths.pathName
-        // Refer to the Pedro Pathing Docs (Auto Example) for an example state machine
         return pathState;
     }
     public void setPathState(int pState) {
         pathState = pState;
         pathTimer.resetTimer();
-
     }
 }
-
