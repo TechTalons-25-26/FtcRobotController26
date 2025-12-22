62cac4bf51068f9e8c2d8fb5be9787327ed9d378
Kanishk Deshmukh <kanishk.deshmukh10@gmail.com>
2025-12-22 14:20:37 -0800

    unfinished changes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/qt2Auto/smallBlueFuture.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/qt2Auto/smallBlueFuture.java
@@ -12,7 +12,7 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import org.firstinspires.ftc.teamcode.pedroPathing.Constants;
 
-@Autonomous(name = "bigBlueCurrent", group = "Autonomous")
+@Autonomous(name = "smallBlueFuture", group = "Autonomous")
 @Configurable // Panels
 public class smallBlueFuture extends OpMode {
 
