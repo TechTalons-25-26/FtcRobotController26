742c103c06f9ee72bcd49ca343b0061899790fd0
Triticism <kanishk.deshmukh10@gmail.com>
2025-12-24 12:08:38 -0800

    i forgot to add if follower is busy

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/qt2Auto/bigBlue.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/qt2Auto/bigBlue.java
@@ -218,74 +218,103 @@ public class bigBlue extends OpMode {
 
     public void statePathUpdate() {
         switch (pathState) {
+
             case BIGBLUESTART_BLUESHOOT:
-                follower.followPath(bigBlueStart_blueShoot, true);
+                if(!follower.isBusy()) {
+                    follower.followPath(bigBlueStart_blueShoot, true);
                 setPathState(PathState.BLUESHOOT_BLUETOPSTART);
                 break;
+                }
 
             case BLUESHOOT_BLUETOPSTART:
+                if(!follower.isBusy()) {
                 follower.followPath(blueShoot_blueTopStart, true);
                 setPathState(PathState.BLUETOPSTART_BLUETOPEND);
                 break;
+                }
 
             case BLUETOPSTART_BLUETOPEND:
+                if(!follower.isBusy()) {
                 follower.followPath(blueTopStart_blueTopEnd, true);
                 setPathState(PathState.BLUETOPEND_BLUETOPSTART);
                 break;
+                }
 
             case BLUETOPEND_BLUETOPSTART:
+                if(!follower.isBusy()) {
                 follower.followPath(blueTopEnd_blueTopStart, true);
                 setPathState(PathState.BLUETOPSTART_BLUESHOOT);
                 break;
+                }
 
             case BLUETOPSTART_BLUESHOOT:
+                if(!follower.isBusy()) {
                 follower.followPath(blueTopStart_blueShoot, true);
                 setPathState(PathState.BLUESHOOT_BLUEMIDDLESTART);
                 break;
+                }
 
             case BLUESHOOT_BLUEMIDDLESTART:
+                if(!follower.isBusy()) {
                 follower.followPath(blueShoot_blueMiddleStart, true);
                 setPathState(PathState.BLUEMIDDLESTART_BLUEMIDDLEEND);
                 break;
+                }
 
             case BLUEMIDDLESTART_BLUEMIDDLEEND:
+                if(!follower.isBusy()) {
                 follower.followPath(blueMiddleStart_blueMiddleEnd, true);
                 setPathState(PathState.BLUEMIDDLEEND_BLUEMIDDLESTART);
                 break;
+                }
 
             case BLUEMIDDLEEND_BLUEMIDDLESTART:
+                if(!follower.isBusy()) {
                 follower.followPath(blueMiddleEnd_blueMiddleStart, true);
                 setPathState(PathState.BLUEMIDDLESTART_BLUESHOOT);
                 break;
+                }
 
             case BLUEMIDDLESTART_BLUESHOOT:
+                if(!follower.isBusy()) {
                 follower.followPath(blueMiddleStart_blueShoot, true);
                 setPathState(PathState.BLUESHOOT_BLUEBOTTOMSTART);
                 break;
+                }
 
             case BLUESHOOT_BLUEBOTTOMSTART:
+                if(!follower.isBusy()) {
                 follower.followPath(blueShoot_blueBottomStart, true);
                 setPathState(PathState.BLUEBOTTOM_BLUEBOTTOMEND);
                 break;
+                }
 
             case BLUEBOTTOM_BLUEBOTTOMEND:
+                if(!follower.isBusy()) {
                 follower.followPath(blueBottom_blueBottomEnd, true);
                 setPathState(PathState.BLUEBOTTOMEND_BLUEBOTTOMSTART);
                 break;
+                }
 
             case BLUEBOTTOMEND_BLUEBOTTOMSTART:
+                if(!follower.isBusy()) {
                 follower.followPath(blueBottomEnd_blueBottomStart, true);
                 setPathState(PathState.BLUEBOTTOMSTART_BLUESHOOT);
                 break;
+                }
 
             case BLUEBOTTOMSTART_BLUESHOOT:
+                if(!follower.isBusy()) {
                 follower.followPath(blueBottomStart_blueShoot, true);
                 setPathState(PathState.BLUESHOOT_BLUEEND);
                 break;
+                }
 
             case BLUESHOOT_BLUEEND:
+                if(!follower.isBusy()) {
                 follower.followPath(blueShoot_blueEnd, true);
                 break;
+                }
         }
 
     }
